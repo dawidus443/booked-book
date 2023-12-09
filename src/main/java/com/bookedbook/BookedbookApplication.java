c36d00525b84e687aaba4c6f5f9409ced454acee
dawidus443 <dawidbadora@gmail.com>
2023-12-09 20:54:38 +0100

    booked book app

---
+++ src/main/java/com/bookedbook/BookedbookApplication.java
@@ -0,0 +1,13 @@
+package com.bookedbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookedbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookedbookApplication.class, args);
+	}
+
+}
